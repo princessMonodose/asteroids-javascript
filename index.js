d44ae886f592e0351338d882db0e5aad5e0fe786
= <=>
2024-01-29 16:16:02 +0100

    create player

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ canvas.height = window.innerHeight;
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.moveTo(this.position.x + 30, this.position.y);
+    ctx.lineTo(this.position.x - 10, this.position.y - 10);
+    ctx.lineTo(this.position.x - 10, this.position.y + 10);
+    ctx.closePath();
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+  }
+}
+
+const player = new Player({
+  position: { x: canvas.width / 2, y: canvas.height / 2 },
+  velocity: { x: 0, y: 0 },
+});
+player.draw();
